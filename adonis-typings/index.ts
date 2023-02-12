a593d71c235fbe97c32cc7eda7b19835903a4837
jeremy <jeremy@chaufourier.fr>
2023-02-12 10:54:14 +0100

    Update header comments

--- adonis-typings/index.ts
+++ adonis-typings/index.ts
@@ -3,6 +3,8 @@
  *
  * (c) Chaufourier Jeremy <jeremy@chaufourier.fr>
  *
+ * Fork of @adonisjs/attachment-lite create by @adonisjs/attachment-lite
+ *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
