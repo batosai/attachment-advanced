e91ad19857d6e3d0a477aef35164d5282e94bc4a
jeremy <jeremy@chaufourier.fr>
2023-07-08 21:46:38 +0200

    wip

--- test/attachment.spec.ts
+++ test/attachment.spec.ts
@@ -288,7 +288,7 @@ test.group('Attachment | fromFile', (group) => {
     const Drive = app.container.resolveBinding('Adonis/Core/Drive')
     assert.isTrue(await Drive.exists(body.name))
     assert.isTrue(await Drive.exists(body.variants.thumbnail.name))
-  })//.disableTimeout()
+  }).disableTimeout()
 
   test('create attachment from the user uploaded file with mp4 file', async ({ assert }) => {
     const server = createServer((req, res) => {
